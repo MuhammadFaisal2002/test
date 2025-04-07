dadc6bfbd6775946ffde70bf47418c422683f390
MF Developer <faisalabshar2002@gmail.com>
2025-04-07 14:32:11 +0500

    Add feature xyz

--- learninggithub/app/page.tsx
+++ learninggithub/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
+import Testing from "./component/Testing";
 
 export default function Home() {
   return (
     <>
     testing changes
+    <Testing/>
     </>
-  );
+  ); 
 }
 
